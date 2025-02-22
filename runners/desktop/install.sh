9047e6c532d131324a6dfa0b67539bf0a3ca5c4b
Radvil <radvil.linux@gmail.com>
2025-02-23 02:31:48 +0800

    pussy999999999x

--- runners/desktop/install.sh
+++ runners/desktop/install.sh
@@ -12,23 +12,23 @@ wget -qO- https://git.io/papirus-folders-install | sh
 
 papirus-folders -C yaru
 
-KDE_PACKAGES=(
-  kvantum
-  breeze-gtk
-)
-
-GNOME_PACKAGES=(
-  gnome-tweaks
-)
-
 LAYERED_PACKAGES=(
-  vinyl-theme
   alacritty
   gparted
   lazygit
   neovim
-  kitty
-  code
+  # kitty
+  # code
+)
+
+KDE_PACKAGES=(
+  kvantum
+  breeze-gtk
+  # vinyl-theme
+)
+
+GNOME_PACKAGES=(
+  gnome-tweaks
 )
 
 # Install layered packages
@@ -36,17 +36,16 @@ echo "=== Installing common packages ==="
 dnf5 install -y "${LAYERED_PACKAGES[@]}"
 
 # Install desktop environment specific packages
-if [[ "${IS_GNOME_VARIANT}" == "1" ]]; then
+echo "=== IS_GNOME_VARIANT: ${IS_GNOME_VARIANT}"
+
+if [[ "${IS_GNOME_VARIANT}" == "true" ]]; then
   echo "=== Installing GNOME packages ==="
   dnf5 install -y "${GNOME_PACKAGES[@]}"
+
+  echo "=== Installing Cosmic Desktop ==="
+  dnf5 copr enable -y ryanabx/cosmic-epoch
+  dnf5 install -y cosmic-desktop
 else
   echo "=== Installing KDE packages ==="
   dnf5 install -y "${KDE_PACKAGES[@]}"
-fi
-
-# Configure desktop environment
-if [[ "${IS_GNOME_VARIANT}" == "1" ]]; then
-  echo "=== Configuring GNOME settings ==="
-  dnf5 copr enable -y ryanabx/cosmic-epoch
-  dnf5 install -y cosmic-desktop
 fi
